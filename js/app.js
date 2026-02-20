dda1160585cf0b3f4500b06b90c75e0ca7071d74
Lorena Gonçalves <lorena.b.goncalves@aluno.senai.br>
2026-02-20 08:51:57 -0300

    modify: alterei os erros de organização e melhorei as frases

--- js/app.js
+++ js/app.js
@@ -10,24 +10,25 @@ function calcularTotal(){
     const v1 = parseFloat(input01.value);
     const v2 = parseFloat(input02.value);
 
+    let total = v1 *v2;
+
     // verifica se é um número
     if (isNaN(v1) || isNaN(v2)){
         resultado.style.display = 'flex';
 
         resultado.innerHTML = `
-            <h2>'Valores incorretos, digite um número para cada campo.'</h2>
+            <h2>Valores incorretos, digite um número para cada campo.</h2>
 
         `
     }else{
         resultado.style.display = 'flex';
 
         resultado.innerHTML = `
-            <h2>Total a pagar</h2>
+            <h2>Total da Compra</h2>
             <ul>
-                <li><span>Soma dos produtos</span> </li>
-                <li>Preço do produto: <span> ${v1} </span> </li>
-                <li>Quantidade: <span> ${v2} </span> </li>       
-                <li>O total da compra será de: <span>R$${v1 * v2}</span> </li>
+                <li>Preço do produto: <span> R$ ${v1.toFixed(2)} </span> </li>
+                <li>Quantidade: <span> R$ ${v2.toFixed(2)} </span> </li>       
+                <li>O total da compra será de: <span>R$ ${total.toFixed(2)}</span> </li>
             </ul>
         `
     }
@@ -45,19 +46,18 @@ function desconto(){
         resultado.style.display = 'flex';
 
         resultado.innerHTML = `
-            <h2>'Valores incorretos, digite um número para cada campo.'</h2>
+            <h2>Valores incorretos, digite um número para cada campo.</h2>
 
         `
     }else{
         resultado.style.display = 'flex';
 
         resultado.innerHTML = `
-            <h2>Total a pagar</h2>
+            <h2>Desconto Aplicado</h2>
             <ul>
-                <li><span>Desconto</span> </li>
-                <li>Foi aplicado um desconto de: <span> ${v1} </span> </li>
-                <li>Sobre o valor de: <span> ${v2} </span> </li>       
-                <li>Valor final com desconto: <span>R$${total}</span> </li>
+                <li>O valor foi: <span> R$ ${v1.toFixed(2)} </span> </li> 
+                <li>O desconto aplicado foi de: <span> ${v2}% </span> </li>      
+                <li>Valor final com desconto: <span>R$ ${total.toFixed(2)}</span> </li>
             </ul>
         `
     }
@@ -77,19 +77,18 @@ function juros(){
         resultado.style.display = 'flex';
 
         resultado.innerHTML = `
-            <h2>'Valores incorretos, digite um número para cada campo.'</h2>
+            <h2>Valores incorretos, digite um número para cada campo.</h2>
 
         `
     }else{
         resultado.style.display = 'flex';
 
         resultado.innerHTML = `
-            <h2>Total a pagar</h2>
+            <h2>Juros Aplicado</h2>
             <ul>
-                <li><span>Acréscimo Aplicado</span> </li>
-                <li>Foi aplicado um acréscimo de: <span> ${v1}% </span> </li>
-                <li>Sobre o valor de: <span> R$${v2} </span> </li>       
-                <li>Valor final com juros: <span>R$${total}</span> </li>
+                <li>O valor foi: <span> R$ ${v1.toFixed(2)} </span> </li>  
+                <li>Foi aplicado um acréscimo de: <span> ${v2}% </span> </li>     
+                <li>Valor final com juros: <span>R$ ${total.toFixed(2)}</span> </li>
             </ul>
         `
     }
@@ -100,25 +99,24 @@ function comissao(){
     const v2 = parseFloat(input02.value);
 
     let  porcentagem = v2 / 100
-    comissao = porcentagem * v1
+    let comissao = porcentagem * v1
 
     if (isNaN(v1) || isNaN(v2)){
         resultado.style.display = 'flex';
 
         resultado.innerHTML = `
-            <h2>'Valores incorretos, digite um número para cada campo.'</h2>
+            <h2>Valores incorretos, digite um número para cada campo.</h2>
 
         `
     }else{
         resultado.style.display = 'flex';
 
         resultado.innerHTML = `
-            <h2>Total a pagar</h2>
+            <h2>Comissão Calculada</h2>
             <ul>
-                <li><span>Comissão Calculada</span> </li>
-                <li>Comissão de: <span> ${v1} </span> </li>
-                <li>Sobre uma venda de: <span> ${v2} </span> </li>       
-                <li>Valor da comissão <span>R$${comissao}</span> </li>
+                <li>Percentual aplicado: <span> ${v1}% </span> </li>
+                <li>Valor da venda: <span> R$ ${v2.toFixed(2)} </span> </li>       
+                <li>Valor final da comissão: <span> R$ ${comissao.toFixed(2)}</span> </li>
             </ul>
         `
     }
@@ -138,19 +136,18 @@ function lucro(){
         resultado.style.display = 'flex';
 
         resultado.innerHTML = `
-            <h2>'Valores incorretos, digite um número para cada campo.'</h2>
+            <h2>Valores incorretos, digite um número para cada campo.</h2>
 
         `
     }else{
         resultado.style.display = 'flex';
 
         resultado.innerHTML = `
-            <h2>Total a pagar</h2>
+            <h2>Lucro Obtido</h2>
             <ul>
-                <li><span>Lucro Obtido</span> </li>
-                <li>Preço de venda: <span> ${v1} </span> </li>
-                <li>Custo: <span> ${v2} </span> </li>       
-                <li>O total do lucro será de: <span>R$${lucro}</span> </li>
+                <li>Preço de venda: <span> R$ ${v1.toFixed(2)} </span> </li>
+                <li>Custo do produto: <span> R$ ${v2.toFixed(2)} </span> </li>       
+                <li>O total do lucro será de: <span>R$ ${lucro.toFixed(2)}</span> </li>
             </ul>
         `
     }
